dc266ba8d72ec4856a43798c39e4dfabc1276800
Annam Raghu Pavan <raghupavanannam@gmail.com>
2025-03-19 13:20:06 -0400

    Create assignment_1.py

---
+++ Python/Strings/Assignments/assignment_1.py
@@ -0,0 +1,9 @@
+my_string = "Python is amazing!"
+
+first_word = my_string[:6]
+amazing_part = my_string[10:17]
+reversed_string = my_string[::-1]
+
+print("First word:", first_word)
+print("Amazing part:", amazing_part)
+print("Reversed string:", reversed_string)
